47006740e671982f9436bec276f3c32eb455e034
mrxkon <mr.xkon@gmail.com>
2021-02-25 20:50:40 +0200

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,12 @@
 module.exports = function( grunt ) {
 	var SOURCE_DIR     = 'src/',
 		BUILD_DIR      = 'build/',
-		BUILD_PACK_DIR = 'build/plugin-tpl',
-		VENDOR_DIR     = 'vendor/',
-		path           = require('path');
+		VENDOR_DIR     = 'vendor/';
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON( 'package.json' ),
 		clean: {
 			all: BUILD_DIR,
-			dynamic: {
-				expand: true,
-				cwd: BUILD_PACK_DIR,
-				src: []
-			}
 		},
 		watch: {
 			options: {
@@ -23,7 +16,7 @@ module.exports = function( grunt ) {
 				files: [
 					SOURCE_DIR + '**',
 				],
-				tasks: ['clean:dynamic', 'copy:dynamic'],
+				tasks: ['clean:all', 'copy:all'],
 				options: {
 					spawn: false,
 				},
@@ -57,12 +50,6 @@ module.exports = function( grunt ) {
 				cwd: VENDOR_DIR + 'composer',
 				src: '**',
 				dest: BUILD_DIR + '<%= pkg.name %>/' + VENDOR_DIR + 'composer',
-			},
-			dynamic: {
-				expand: true,
-				cwd: SOURCE_DIR,
-				dest: BUILD_PACK_DIR,
-				src: []
 			},
 		},
 		compress: {
@@ -102,26 +89,5 @@ module.exports = function( grunt ) {
 			'copy:all',
 			'compress'
 		]
-	);
-
-	grunt.event.on(
-		'watch',
-		function( action, filepath, target ) {
-			var src;
-
-			filepath = filepath.replace( /\\/g, '/' );
-			src      = [ path.relative( SOURCE_DIR, filepath ) ];
-
-			if ( ! src ) {
-				grunt.warn( 'Failed to determine the destination file.' );
-				return;
-			}
-
-			if ( action === 'deleted' ) {
-				grunt.config( [ 'clean', 'dynamic', 'src' ], src );
-			} else {
-				grunt.config( [ 'copy', 'dynamic', 'src' ], src );
-			}
-		}
 	);
 };
